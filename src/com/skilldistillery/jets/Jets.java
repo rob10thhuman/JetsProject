ad5e594288258e45d99831adf35483b87b0d748a
rob10thHuman <rob@10thHuman.com>
2018-09-07 16:11:30 -0600

    Initial commit

---
+++ src/com/skilldistillery/jets/Jets.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.jets;
+
+public abstract class Jets {
+	private String model;
+	private double speed;
+	private double range;
+	private double price;
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public double getRange() {
+		return range;
+	}
+
+	public void setRange(double range) {
+		this.range = range;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Jets() {
+		
+	}
+	public Jets(String model, double speed, double range, double price) {
+		super();
+		this.model = model;
+		this.speed = speed;
+		this.range = range;
+		this.price = price;
+	}
+
+	public void fly() {	
+	}
+}
