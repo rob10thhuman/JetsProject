8a9d57db6f135f2d3cbc2d5e6c3f44cc81187fa0
rob10thHuman <rob@10thHuman.com>
2018-09-08 22:52:24 -0600

    uploading what i have, am a bit lost. Conceptually, I think I know in Englist what I want to do but hitting a roadblock on implementing the mechanics of it. Taking a break will reach out tomorrow for help.

--- src/com/skilldistillery/jets/JetApp.java
+++ src/com/skilldistillery/jets/JetApp.java
@@ -1,54 +1,81 @@
 package com.skilldistillery.jets;
 
+import java.util.Scanner;
+
 public class JetApp {
 
 	public static void main(String[] args) {
-		JetApp letsRoll = new JetApp(); 
-		letsRoll.userMenu();
-		letsRoll.run(); 
-		letsRoll.listFleet(); 
-	}
-	
-
-	private void listFleet() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	private void userMenu() {
-//		List fleet
-//		Fly all jets
-//		View fastest jet
-//		View jet with longest range
-//		Load all Cargo Jets
-//		Dogfight!
-//		Add a jet to Fleet
-//		Quit
-		System.out.println("MENU");
-		
+		JetApp letsRoll = new JetApp();
+		letsRoll.userMenu(); 
 	}
 
 	private void run() {
-		Airfield petersonAFB = new Airfield(); 
+		Airfield petersonAFB = new Airfield();
 		petersonAFB.createJets();
-		Jet[] petersonJets = petersonAFB.jetInventory; 
+		Jet[] petersonJets = petersonAFB.jetInventory;
 		
-		for (int i = 0; i < petersonJets.length; i++) {
-			if(petersonJets[i]!= null) {
-				System.out.println(petersonJets[i].getModel());
-				System.out.println(petersonJets[i].getPrice());
-				System.out.println(petersonJets[i].getRange());
-			}
+//		userMenu();
+
+//		for (int i = 0; i < petersonJets.length; i++) {
+//			if (petersonJets[i] != null) {
+//				System.out.println(petersonJets[i].getModel());
+//				System.out.println(petersonJets[i].getPrice());
+//				System.out.println(petersonJets[i].getRange());
+//			}
+//		}
+	}
+
+	private void listFleet() {
+		System.out.println("MENU");
+	
+//		for (int i = 0; i < petersonJets.length; i++) {
+//			if (petersonJets[i] != null) {
+//				System.out.println(petersonJets[i].getModel());
+//				System.out.println(petersonJets[i].getPrice());
+//				System.out.println(petersonJets[i].getRange());
+//			}
+	}
+
+	private void userMenu() {
+
+		Scanner scanner = new Scanner(System.in);
+		int choice = scanner.nextInt();
+
+		System.out.println("Welcome to the Airfield!");
+		System.out.println("Here are your options!");
+		System.out.println("1 - List fleet");
+		System.out.println("2 - Fly all jets");
+		System.out.println("3 - View jet with the longest range");
+		System.out.println("4 - Load all Cargo Jets");
+		System.out.println("5 - Dogfight!");
+		System.out.println("6 - Add a jet to the fleet");
+		System.out.println("7 - Quit");
+
+		switch (choice) {
+		case 1:
+			listFleet();
+//			break;
+		case 2:
+			// Perform "encrypt number" case.
+//			break;
+		case 3:
+			// Perform "decrypt number" case.
+//			break;
+		case 4:
+			// Perform "quit" case.
+//			break;
+		default:
+			// The user input an unexpected choice.
 		}
-		
-		
+
+	}
+
 //		C130 hercules = new C130("C130", 368, 2361, 30); 
 //		C5 starlifter = new C5("C5", 500, 1498, 225); 
 //		F16 viper = new F16("F16", 1500, 2622, 19); 
 //		F22 raptor = new F22("F22", 1498, 1839, 339); 
 //		F35 lightning = new F35("F35", 1550, 1200, 95); 
-		
+
 //		FighterJet fighterJet = new FighterJet();
 //		Jet[] jetInventory = new Jet[10]; 
 //		jetInventory[0]=new FighterJet("F16",1500,2622,19);
@@ -56,10 +83,5 @@ public class JetApp {
 //		jetInventory[2]=new FighterJet("F35",1550,1200,95);
 //		jetInventory[3]=new CargoPlane("C130",368,2361,30);
 //		jetInventory[4]=new FighterJet("C5",1550,7273,225);
-		
-		
-		System.out.println("Test!");
-		
-	}
-	
+
 }
